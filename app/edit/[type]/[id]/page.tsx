a1714f62bbf7975a16fcc0525d3916c041210d70
najaf.shafi <najafshafee5@gmail.com>
2025-04-13 22:45:54 +0500

    feat: improved modal handling and refactored dispute details display

--- app/edit/[type]/[id]/page.tsx
+++ app/edit/[type]/[id]/page.tsx
@@ -1,16 +1,24 @@
-"use client";
+// "use client";
+
+// import React from "react";
+// import NewJob from "../../../post-new-job/NewJob";
+
+// export default function EditJobPage({
+//   params,
+// }: {
+//   params: { id?: string; type?: string };
+// }) {
+//   return (
+//     <div className="pt-[90px] bg-secondary h-[150vh]">
+//       <NewJob params={params} />
+//     </div>
+//   );
+// }
 
 import React from "react";
-import NewJob from "../../../post-new-job/NewJob";
 
-export default function EditJobPage({
-  params,
-}: {
-  params: { id?: string; type?: string };
-}) {
-  return (
-    <div className="pt-[90px] bg-secondary h-[150vh]">
-      <NewJob params={params} />
-    </div>
-  );
-} 
+const page = () => {
+  return <div>Hello</div>;
+};
+
+export default page;
