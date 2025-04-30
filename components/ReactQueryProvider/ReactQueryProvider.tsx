e9c60ae0337b65d0aed93921a059716d1197fe2a
najaf.shafi <najafshafee5@gmail.com>
2025-05-01 00:23:09 +0500

    refactor: update ReactQueryProvider to improve query client configuration and adjust stale time settings

--- components/ReactQueryProvider/ReactQueryProvider.tsx
+++ components/ReactQueryProvider/ReactQueryProvider.tsx
@@ -1,30 +1,3 @@
-// "use client";
-
-// import { QueryClient, QueryClientProvider } from "react-query";
-// import { useState } from "react";
-
-// export default function ReactQueryProvider({
-//   children,
-// }: {
-//   children: React.ReactNode;
-// }) {
-//   const [queryClient] = useState(
-//     () =>
-//       new QueryClient({
-//         defaultOptions: {
-//           queries: {
-//             refetchOnWindowFocus: false, // Disable refetching on tab focus
-//             staleTime: 2 * 60 * 1000, // Consider data fresh for 2 minutes
-//           },
-//         },
-//       })
-//   );
-
-//   return (
-//     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-//   );
-// }
-
 "use client";
 
 import { QueryClient, QueryClientProvider } from "react-query";
@@ -35,9 +8,37 @@ export default function ReactQueryProvider({
 }: {
   children: React.ReactNode;
 }) {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+            staleTime: 30 * 1000, // Consider data fresh for 5 seconds
+            refetchOnMount: true, // Ensure fetch on mount if no data
+          },
+        },
+      })
+  );
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
 }
+
+// "use client";
+
+// import { QueryClient, QueryClientProvider } from "react-query";
+// import { useState } from "react";
+
+// export default function ReactQueryProvider({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   const [queryClient] = useState(() => new QueryClient());
+
+//   return (
+//     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+//   );
+// }
