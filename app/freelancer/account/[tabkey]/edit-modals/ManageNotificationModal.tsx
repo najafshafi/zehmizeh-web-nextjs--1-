a1416708cfd912d1f68be3e6e72ff50501b42972
najaf.shafi <najafshafee5@gmail.com>
2025-05-08 20:57:38 +0500

    refactor: replace styled-components with Headless UI Dialog for modals, enhancing consistency and accessibility across the application

--- app/freelancer/account/[tabkey]/edit-modals/ManageNotificationModal.tsx
+++ app/freelancer/account/[tabkey]/edit-modals/ManageNotificationModal.tsx
@@ -1,20 +1,7 @@
-import { Modal, Button } from "react-bootstrap";
-import { StyledModal } from "@/components/styled/StyledModal";
-import { StyledButton } from "@/components/forms/Buttons";
-import styled from "styled-components";
+import { useEffect, useState, Fragment } from "react";
+import { Dialog, Transition } from "@headlessui/react";
 import Select from "react-select";
-import { useEffect, useState } from "react";
 import LoadingButtons from "@/components/LoadingButtons";
-
-const Wrapper = styled.div`
-  .close-btn {
-    border: 1px solid ${(props) => props.theme.statusColors.darkPink.color};
-    color: ${(props) => props.theme.statusColors.darkPink.color};
-    :hover {
-      background-color: inherit;
-    }
-  }
-`;
 
 // Define option type for React-Select
 interface OptionType {
@@ -61,42 +48,70 @@ const ManageNotificationModal = ({
   }, [show, defaultEmailNotification]);
 
   return (
-    <StyledModal maxwidth={678} show={show} size="sm" onHide={toggle} centered>
-      <Modal.Body>
-        <Button variant="transparent" className="close" onClick={toggle}>
-          &times;
-        </Button>
-        <Wrapper>
-          <div className="content flex flex-col">
-            <div className="modal-title fs-28 font-normal">
-              Manage Notification
-            </div>
-            <Select
-              defaultValue={selectedOPT}
-              value={selectedOPT}
-              className="mt-3"
-              options={options}
-              onChange={(dt) => setSelectedOPT(dt as OptionType)}
-            />
-          </div>
-          <div className="mt-4 flex items-center justify-center">
-            <StyledButton
-              disabled={loading}
-              style={{ minWidth: "10rem" }}
-              onClick={() => onConfirm(selectedOPT)}
+    <Transition appear show={show} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={toggle}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black bg-opacity-25" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
             >
-              {loading ? (
-                <>
-                  <LoadingButtons />
-                </>
-              ) : (
-                "Save"
-              )}
-            </StyledButton>
+              <Dialog.Panel className="w-full max-w-[678px] transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                <button
+                  type="button"
+                  className="absolute top-3 right-3 text-3xl font-normal text-gray-400 hover:text-gray-600"
+                  onClick={toggle}
+                >
+                  &times;
+                </button>
+
+                <div>
+                  <div className="content flex flex-col">
+                    <Dialog.Title className="text-[28px] font-normal">
+                      Manage Notification
+                    </Dialog.Title>
+                    <Select
+                      defaultValue={selectedOPT}
+                      value={selectedOPT}
+                      className="mt-3"
+                      options={options}
+                      onChange={(dt) => setSelectedOPT(dt as OptionType)}
+                    />
+                  </div>
+                  <div className="mt-4 flex items-center justify-center">
+                    <button
+                      type="button"
+                      disabled={loading}
+                      className="min-w-[10rem] px-4 py-2 rounded-md bg-pink-600 text-white hover:bg-pink-700 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed"
+                      onClick={() => onConfirm(selectedOPT)}
+                    >
+                      {loading ? <LoadingButtons /> : "Save"}
+                    </button>
+                  </div>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
           </div>
-        </Wrapper>
-      </Modal.Body>
-    </StyledModal>
+        </div>
+      </Dialog>
+    </Transition>
   );
 };
 
